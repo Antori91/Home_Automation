d633ecf3dface0a362544d4be1327be2220ee2a7
Antori91 <35433970+Antori91@users.noreply.github.com>
2018-06-07 09:12:01 +0200

    Add files via upload

--- WiFi_OTA_MQTT_SecretKeys.h
+++ WiFi_OTA_MQTT_SecretKeys.h
@@ -8,5 +8,15 @@
 #define WIFI_4_PWD        "Your_Password_For_SSID_4"
 #define MQTT_SERVER_IP    "192.168.XX.XX"
 #define OTA_PWD           "Your_Password_For_OTA"
-
+#define OUTDOOR_TEMP_IP   IPAddress(192,168,X,XXX)
+#define INDOOR_TEMP_IP    IPAddress(192,168,X,XXX) 
+#define NEW_TEMP_IP       IPAddress(192,168,X,XXX)
+#define OUTDOOR_TEMP_ID   "YOUR SENSOR ID1"
+#define INDOOR_TEMP_ID    "YOUR SENSOR ID2"
+#define NEW_TEMP_ID       "YOUR SENSOR ID3"
+#ifdef FIXED_IP_USED
+#include <ESP8266WiFi.h>
+IPAddress gateway(192,168,X,X);
+IPAddress subnet(255,255,255,0);
+#endif
 
