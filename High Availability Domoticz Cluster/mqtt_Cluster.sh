22a0446ec0da734541b52fefe7e52c8cb5a9d4a4
Antori91 <35433970+Antori91@users.noreply.github.com>
2018-08-10 14:55:17 +0200

    Add files via upload

--- High Availability Domoticz Cluster/mqtt_Cluster.sh
+++ High Availability Domoticz Cluster/mqtt_Cluster.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd /home/pi/iot_domoticz
-node ./mqtt_Cluster.js > /home/pi/Samba/domoticz/mqtt_Cluster.log 2> /home/pi/Samba/domoticz/mqtt_Cluster.log &
+node ./mqtt_Cluster.js >> /home/pi/Samba/domoticz/mqtt_Cluster.log 2>> /home/pi/Samba/domoticz/mqtt_Cluster.log &
+
