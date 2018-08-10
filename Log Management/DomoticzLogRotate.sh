6585a97de278b1e941e58d84ac52afed7bea2c3b
Antori91 <35433970+Antori91@users.noreply.github.com>
2018-08-10 15:51:07 +0200

    Add files via upload

--- Log Management/DomoticzLogRotate.sh
+++ Log Management/DomoticzLogRotate.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 cd /volume1/@appstore/iot_domoticz
 cp /volume1/@appstore/domoticz/var/backups/hourly/backup-hour-23.db /volume1/sauvegarde/ONEDRIVE_DOMOTICZ
-echo -e "==\n== LAST DOMOTICZ ACTIVITIES ==\n=="  > /volume1/Alarme/DomoticZ/domoticzEvents.log
+echo -e "==\n== LAST MAIN DOMOTICZ SERVER ACTIVITIES ==\n=="  > /volume1/Alarme/DomoticZ/domoticzEvents.log
 tail /volume1/@appstore/domoticz/var/domoticz.log >> /volume1/Alarme/DomoticZ/domoticzEvents.log
-echo -e "==\n== MAJOR DEVICE EVENTS ==\n=="  >> /volume1/Alarme/DomoticZ/domoticzEvents.log
+echo -e "==\n== MAJOR MAIN DOMOTICZ and DEVICE EVENTS ==\n=="  >> /volume1/Alarme/DomoticZ/domoticzEvents.log
 /bin/grep -i -a -e DHT22 -e webserver -e opening -e Heater -e Lighting -e ALARM -e HotWaterTank -e incoming -e login /volume1/@appstore/domoticz/var/domoticz.log >> /volume1/Alarme/DomoticZ/domoticzEvents.log
-echo -e "==\n== ALARM SERVER/ERROR EVENTS ==\n==" >> /volume1/Alarme/DomoticZ/domoticzEvents.log
+echo -e "==\n== CLUSTER WARNING/ERROR EVENTS ==\n==" >> /volume1/Alarme/DomoticZ/domoticzEvents.log
+/bin/grep -i -e FAILURE -e ALERT /volume1/Alarme/mtCluster_DMOTIC_BACKUP/domoticz/mqtt_Cluster.log >> /volume1/Alarme/DomoticZ/domoticzEvents.log
+echo -e "==\n== ALARM SERVER WARNING/ERROR EVENTS ==\n==" >> /volume1/Alarme/DomoticZ/domoticzEvents.log
 /bin/grep -i -e FAILURE /volume1/Alarme/RaspberrySamba/iot_ALARM-SVR.log >> /volume1/Alarme/DomoticZ/domoticzEvents.log
 echo -e "==\n== DECOMMISSIONED PUBNUB ALARM SERVER/PUBNUB WARNING/ERROR EVENTS ==\n==" >> /volume1/Alarme/DomoticZ/domoticzEvents.log
 /bin/grep -h -r -e RECONNECT /volume1/Alarme/iot_CVQ6081-SecPanel >> /volume1/Alarme/DomoticZ/domoticzEvents.log
