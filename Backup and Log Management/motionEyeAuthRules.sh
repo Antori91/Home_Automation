c30b6c0e040f905525c2c2f8d65bc012954db0b0
Antori91 <35433970+Antori91@users.noreply.github.com>
2019-01-28 11:17:56 +0100

    Update motionEyeAuthRules.sh

--- Backup and Log Management/motionEyeAuthRules.sh
+++ Backup and Log Management/motionEyeAuthRules.sh
@@ -2,7 +2,7 @@
 # @Antori91  http://www.domoticz.com/forum/memberlist.php?mode=viewprofile&u=13749
 # ***** motionEye incoming connections tracking *****
 # V0.1 - January 2019 - Initial version
-sudo iptables -I INPUT -p tcp --dport 8765 --syn  -m limit --limit 4/minute -j LOG --log-prefix "MOTIONEYE: "
+sudo iptables -I INPUT -p tcp --dport [YOUR_MOTIONEYE_PORT_NUMBER] --syn  -m limit --limit 4/minute -j LOG --log-prefix "MOTIONEYE: "
 sudo iptables -L
 
 
