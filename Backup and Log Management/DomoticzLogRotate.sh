29560f90af183e74046a674f6a440acc51505ff4
Antori91 <35433970+Antori91@users.noreply.github.com>
2019-01-28 11:07:57 +0100

    Add files via upload

--- Backup and Log Management/DomoticzLogRotate.sh
+++ Backup and Log Management/DomoticzLogRotate.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # @Antori91  http://www.domoticz.com/forum/memberlist.php?mode=viewprofile&u=13749
 # ***** Domoticz database backup and log management *****
+# V0.7 - January 2019 - motionEye incoming connections added
 # V0.6 - December 2018 - Domoticz automatic backup not used anymore. 
-MAIN_SERVER_IP="192.168.1.XXX"   # Main Domoticz server
-BACKUP_SERVER_IP="192.168.1.XXX" # Backup Domoticz server
-DZ_PORT="XXXX"                   # Domoticz IP port 
+MAIN_SERVER_IP="192.168.1.45"    # Main Domoticz server
+BACKUP_SERVER_IP="192.168.1.104" # Backup Domoticz server
+DZ_PORT="8084"                   # Domoticz IP port 
 MAIN_DZ_BACKUPFILE="/volume1/Alarme/Dz_DBase_Backup/MainDzSvr.db"
 BACKUP_DZ_BACKUPFILE="/volume1/Alarme/Dz_DBase_Backup/BackupDzSvr.db"          
 curl -s "http://$MAIN_SERVER_IP:$DZ_PORT/json.htm?type=command&param=addlogmessage&message=Starting%20manual%20database%20backup%20procedure..."
@@ -26,6 +27,8 @@ echo -e "==\n== CLUSTER WARNING/ERROR EVENTS ==\n==" >> /volume1/Alarme/DomoticZ
 /bin/grep -i -e FAILURE -e ALERT /volume1/Alarme/mtCluster_DMOTIC_BACKUP/domoticz/mqtt_Cluster.log >> /volume1/Alarme/DomoticZ/domoticzEvents.log
 echo -e "==\n== ALARM SERVER WARNING/ERROR EVENTS ==\n==" >> /volume1/Alarme/DomoticZ/domoticzEvents.log
 /bin/grep -i -e FAILURE /volume1/Alarme/RaspberrySamba/iot_ALARM-SVR.log >> /volume1/Alarme/DomoticZ/domoticzEvents.log
+echo -e "==\n== MOTIONEYE INCOMING CONNECTIONS ==\n==" >> /volume1/Alarme/DomoticZ/domoticzEvents.log
+cat /volume1/Alarme/RaspberrySamba/iptables.log >> /volume1/Alarme/DomoticZ/domoticzEvents.log
 echo -e "==\n== DECOMMISSIONED PUBNUB ALARM SERVER/PUBNUB WARNING/ERROR EVENTS ==\n==" >> /volume1/Alarme/DomoticZ/domoticzEvents.log
 /bin/grep -h -r -e RECONNECT /volume1/Alarme/iot_CVQ6081-SecPanel >> /volume1/Alarme/DomoticZ/domoticzEvents.log
 /bin/grep -i -e ERROR /volume1/Alarme/RaspberrySamba/iot_CVQ6081.log >> /volume1/Alarme/DomoticZ/domoticzEvents.log
---
+++ Backup and Log Management/motionEyeAuthRules.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# @Antori91  http://www.domoticz.com/forum/memberlist.php?mode=viewprofile&u=13749
+# ***** motionEye incoming connections tracking *****
+# V0.1 - January 2019 - Initial version
+sudo iptables -I INPUT -p tcp --dport 8765 --syn  -m limit --limit 4/minute -j LOG --log-prefix "MOTIONEYE: "
+sudo iptables -L
+
+
+
