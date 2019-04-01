3065a6b72cc9a26ca33047a4d956a3a150ec4d98
Antori91 <35433970+Antori91@users.noreply.github.com>
2019-04-01 16:41:28 +0200

    Add files via upload

--- Domoticz server side scripts/WiFi_DZ_MQTT_SecretKeys.js
+++ Domoticz server side scripts/WiFi_DZ_MQTT_SecretKeys.js
@@ -1,4 +1,6 @@
 // @Antori91  http://www.domoticz.com/forum/memberlist.php?mode=viewprofile&u=13749
+// V0.5 - April 2019
+// Fire Alarm IDX added
 // V0.4 - February 2019
 // Cluster Main server SSH Port/User/Pwd added 
 // V0.3 - August 2018 
@@ -6,7 +8,7 @@
 var mySecretKeys = {
 MAIN_SERVER_HARDWARE       : 'SYNOLOGY',      // Main Domoticz server Hardware: SYNOLOGY and RASPBERRY supported
 MAIN_SERVER_IP             : '192.168.1.XX',  // Main Domoticz server IP address
-MAIN_SERVER_SSH_PORT       :  XXX,            // Main Domoticz server SSH Port
+MAIN_SERVER_SSH_PORT       :  XXXX,           // Main Domoticz server SSH Port
 MAIN_SERVER_SSH_USER       : '----------',    // Main Domoticz server SSH User
 MAIN_SERVER_ROOT_PWD       : '----------',    // Main Domoticz server root Password
 BACKUP_SERVER_HARDWARE     : 'RASPBERRY',     // Backup Domoticz server Hardware
@@ -22,10 +24,13 @@ idx_UnactivateHeaters      : '23',                               //[$$DOMOTICZ_P
 Var_UnactiveHeaters        : '1',                                //[$$DOMOTICZ_PARAMETER] - Dz user variable# to store the heaters heating state command
 Varname_UnactiveHeaters    : 'HeatersActive',                    //[$$DOMOTICZ_PARAMETER] - Dz user variable name to store the heaters heating state command
 ProtectedDevicePassword    : 'XXXX',                             //[$$DOMOTICZ_PARAMETER] - Dz password for protected device
-idx_SecPanel               : 'X',                                //[$$DOMOTICZ_PARAMETER] - Dz Security Panel
+idx_SecPanel               : 'XX',                               //[$$DOMOTICZ_PARAMETER] - Dz Security Panel
 SecPanel_Seccode           : '------------------------------',   //[$$DOMOTICZ_PARAMETER] - Dz Security Panel Password MD5 Encrypted 
-idx_AlarmALERT             : '7',                                //[$$DOMOTICZ_PARAMETER] - Alarm Alert status - Dz "ALERTE Intrusion Maison" Device 
-idx_AlarmARM               : '41'                                //[$$DOMOTICZ_PARAMETER] - Alarm Armed status - Dz "Maison sous Alarme" Device
+idx_AlarmALERT             : 'XX',                                //[$$DOMOTICZ_PARAMETER] - Alarm Alert status - Dz "ALERTE Intrusion Maison" Device 
+idx_AlarmARM               : 'XX',                               //[$$DOMOTICZ_PARAMETER] - Alarm Armed status - Dz "Maison sous Alarme" Device
+idx_FireALarmStatus        : 'XX',                               //[$$DOMOTICZ_PARAMETER] - Fire Alarm device IDX (Selector)
+idx_FireAlarmTempHum       : '66',                               //[$$DOMOTICZ_PARAMETER] - Fire Alarm temp/humidity device IDX
+idx_FireAlarmHeatidx       : '67'                                //[$$DOMOTICZ_PARAMETER] - Fire Alarm temp heat index device IDX
 };
 
 module.exports   = mySecretKeys;
