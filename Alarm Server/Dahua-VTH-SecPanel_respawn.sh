cf08efa06bedacf55a2164aa27899adaa567d5d5
Antori91 <35433970+Antori91@users.noreply.github.com>
2020-03-28 18:28:18 +0100

    Add files via upload

--- Alarm Server/Dahua-VTH-SecPanel_respawn.sh
+++ Alarm Server/Dahua-VTH-SecPanel_respawn.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # "./Dahua-VTH-SecPanel_respawn &" to run this script
 cd /home/pi/iot_domoticz
+sudo tcpdump --number -n -l 'tcp[tcpflags] & tcp-syn != 0 and dst 192.168.X.XXX' > /home/pi/iot_domoticz/Dahua-VTH-BOX-cxion.log &
+#tcpdump_PID=$!
 until python3 ./Dahua-VTH-SecPanel.py >> /home/pi/Samba/domoticz/Dahua-VTH-SecPanel.log 2>> /home/pi/Samba/domoticz/Dahua-VTH-SecPanel.log; do
 echo "Server 'Dahua-VTH-SecPanel' crashed with exit code $?. Respawning.." >&2
 sleep 1
 done
-
+#sudo kill -9 $tcpdump_PID
