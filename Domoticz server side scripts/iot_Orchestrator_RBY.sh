e5fec1e4d2bedfb3982388ddb502820583376038
Antori91 <35433970+Antori91@users.noreply.github.com>
2020-04-01 10:56:24 +0200

    Add files via upload

--- Domoticz server side scripts/iot_Orchestrator_RBY.sh
+++ Domoticz server side scripts/iot_Orchestrator_RBY.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+# "./iot_Orchestrator_RBY.sh &" to run this script
 cd /home/pi/iot_domoticz
-node ./iot_Orchestrator.js RASPBERRY > /home/pi/Samba/domoticz/iot_Orchestrator.log 2> /home/pi/Samba/domoticz/iot_Orchestrator.log &
+until node ./iot_Orchestrator.js RASPBERRY >> /home/pi/Samba/domoticz/iot_Orchestrator.log 2>> /home/pi/Samba/domoticz/iot_Orchestrator.log; do
+echo "Server 'iot_Orchestrator' crashed with exit code $?. Respawning.." >&2
+sleep 1
+done
+
