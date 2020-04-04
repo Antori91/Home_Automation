e2675285ca606851c27a389a0f0e072e10ba57fe
Antori91 <35433970+Antori91@users.noreply.github.com>
2020-04-04 14:39:24 +0200

    Add files via upload

--- Alarm Server/Dahua-VTH-SecPanel_respawn.sh
+++ Alarm Server/Dahua-VTH-SecPanel_respawn.sh
@@ -4,6 +4,8 @@ cd /home/pi/iot_domoticz
 # Next line, dst 192.168.X.XXX has to be updated to your VTH IP address
 sudo tcpdump --number -n -l 'tcp[tcpflags] & tcp-syn != 0 and dst 192.168.X.XXX' > /home/pi/iot_domoticz/Dahua-VTH-BOX-cxion.log &
 #tcpdump_PID=$!
+export TERM=xterm
+export TERMINFO=/etc/terminfo
 until python3 ./Dahua-VTH-SecPanel.py >> /home/pi/Samba/domoticz/Dahua-VTH-SecPanel.log 2>> /home/pi/Samba/domoticz/Dahua-VTH-SecPanel.log; do
 echo "Server 'Dahua-VTH-SecPanel' crashed with exit code $?. Respawning.." >&2
 sleep 1
