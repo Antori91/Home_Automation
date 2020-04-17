3349ff4c8b054362fbfcb69859b8919a050c64ad
Antori91 <35433970+Antori91@users.noreply.github.com>
2020-04-17 17:33:43 +0200

    Add files via upload

--- Backup and Log Management/DomoticzLogRotate.sh
+++ Backup and Log Management/DomoticzLogRotate.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # @Antori91  http://www.domoticz.com/forum/memberlist.php?mode=viewprofile&u=13749
 # ***** Domoticz database backup and log management *****
+# V1.0 - April 2020    - x
 # V0.95- May 2019      - Main server IP changed to 127.0.0.1
 # V0.9 - April 2019    - Grep in Domoticz logfile MSG instead of Heater, Lighting, ALARM and HotWaterTank
 #                      - Tail -n25 instead of cat the Domoticz and motionEye connections
@@ -9,7 +10,7 @@
 #                      - Domoticz cluster and motionEye reverse proxy incoming connections added
 # V0.7 - January 2019  - motionEye incoming connections added
 # V0.6 - December 2018 - Domoticz automatic backup not used anymore. 
-MAIN_SERVER_IP="127.0.0.1"              # Main Domoticz server
+MAIN_SERVER_IP="127.0.0.1"                 # Main Domoticz server
 BACKUP_SERVER_IP="192.168.x.xxx"        # Backup Domoticz server
 DZ_PORT="9999"                          # Domoticz IP port 
 BACKUP_SERVER_USER="-------"            # Domoticz backup server user name
@@ -41,9 +42,10 @@ tail -n25 /var/log/nginx/mEye.log >> /volume1/Alarme/DomoticZ/domoticzEvents.log
 cat /volume1/Alarme/RaspberrySamba/iptables.log >> /volume1/Alarme/DomoticZ/domoticzEvents.log
 echo -e "==\n== ALARM SERVER WARNING/ERROR EVENTS ==\n==" >> /volume1/Alarme/DomoticZ/domoticzEvents.log
 /bin/grep -i -e FAILURE -e dzSYNC /volume1/Alarme/RaspberrySamba/iot_ALARM-SVR.log | tail -n25 >> /volume1/Alarme/DomoticZ/domoticzEvents.log
-echo -e "==\n== DECOMMISSIONED PUBNUB ALARM SERVER/PUBNUB WARNING/ERROR EVENTS ==\n==" >> /volume1/Alarme/DomoticZ/domoticzEvents.log
-/bin/grep -h -r -e RECONNECT /volume1/Alarme/iot_CVQ6081-SecPanel >> /volume1/Alarme/DomoticZ/domoticzEvents.log
-/bin/grep -i -e ERROR /volume1/Alarme/RaspberrySamba/iot_CVQ6081.log >> /volume1/Alarme/DomoticZ/domoticzEvents.log
-cat /volume1/Alarme/RaspberrySamba/*.pubnub.log >> /volume1/Alarme/DomoticZ/domoticzEvents.log
+/bin/grep -i -a -e "\[-]" -e "\[!]" /volume1/Alarme/mtCluster_DMOTIC_BACKUP/domoticz/Dahua-VTH-SecPanel.log | tail -n25 >> /volume1/Alarme/DomoticZ/domoticzEvents.log
+# echo -e "==\n== DECOMMISSIONED PUBNUB ALARM SERVER/PUBNUB WARNING/ERROR EVENTS ==\n==" >> /volume1/Alarme/DomoticZ/domoticzEvents.log
+# /bin/grep -h -r -e RECONNECT /volume1/Alarme/iot_CVQ6081-SecPanel >> /volume1/Alarme/DomoticZ/domoticzEvents.log
+# /bin/grep -i -e ERROR /volume1/Alarme/RaspberrySamba/iot_CVQ6081.log >> /volume1/Alarme/DomoticZ/domoticzEvents.log
+# cat /volume1/Alarme/RaspberrySamba/*.pubnub.log >> /volume1/Alarme/DomoticZ/domoticzEvents.log
 logrotate ./DomoticzLogRotate.conf -v -f
 
