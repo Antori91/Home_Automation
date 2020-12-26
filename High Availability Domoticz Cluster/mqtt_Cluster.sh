fa43126983595ee9b32bafce8a41c5b50c60eea7
Antori91 <35433970+Antori91@users.noreply.github.com>
2020-12-26 12:07:18 +0100

    Add files via upload

--- High Availability Domoticz Cluster/mqtt_Cluster.sh
+++ High Availability Domoticz Cluster/mqtt_Cluster.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 cd /home/pi/iot_domoticz
-node ./mqtt_Cluster.js >> /home/pi/Samba/domoticz/mqtt_Cluster.log 2>> /home/pi/Samba/domoticz/mqtt_Cluster.log &
-
+until node ./mqtt_Cluster.js >> /home/pi/Samba/domoticz/mqtt_Cluster.log 2>> /home/pi/Samba/domoticz/mqtt_Cluster.log; do
+echo "Server 'mqtt_Cluster' crashed with exit code $?. Respawning.." >&2
+sleep 1
+done
