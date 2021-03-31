da2d042bb984e06030ba0d607efa3163be0c64a5
Antori91 <35433970+Antori91@users.noreply.github.com>
2021-03-31 14:57:48 +0200

    Update iot_ALARM-SVR.js

--- Alarm Server/iot_ALARM-SVR.js
+++ Alarm Server/iot_ALARM-SVR.js
@@ -2,6 +2,5 @@
 // ***** iot_ALARM-SVR.js - Script to manage the CVQ6081 wired Alarm appliance : 
 //       - arm and disarm the Alarm using the Raspberry GPIO/Relay as a keyswitch
 //       - get alarm Alert state at CVQ6081 backpanel
-// V0.11 - August 2018 - Initial release
 
-== CODE AVAILABLE ON DEMAND ==
+https://github.com/Antori91/Home_Alarm/blob/main/Alarm_nodeJS/iot_ALARM-SVR.js
