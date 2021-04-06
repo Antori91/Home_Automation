7408ed2ef53ebc7a29d0200abee8d2d4d07f3bc6
Antori91 <35433970+Antori91@users.noreply.github.com>
2021-04-06 17:15:18 +0200

    Delete iot_ALARM-SVR.js

--- Alarm Server/iot_ALARM-SVR.js
+++
@@ -1,6 +0,0 @@
-// @Antori91  http://www.domoticz.com/forum/memberlist.php?mode=viewprofile&u=13749
-// ***** iot_ALARM-SVR.js - Script to manage the CVQ6081 wired Alarm appliance : 
-//       - arm and disarm the Alarm using the Raspberry GPIO/Relay as a keyswitch
-//       - get alarm Alert state at CVQ6081 backpanel
-
-https://github.com/Antori91/Home_Alarm/blob/main/Alarm_nodeJS/iot_ALARM-SVR.js
