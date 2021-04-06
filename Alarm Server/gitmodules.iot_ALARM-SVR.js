ece48c9279736bbc188816c2c14a0c121aed92a0
Antori91 <35433970+Antori91@users.noreply.github.com>
2021-04-06 17:14:36 +0200

    Add files via upload

---
+++ Alarm Server/gitmodules.iot_ALARM-SVR.js
@@ -0,0 +1,9 @@
+# @Antori91  http://www.domoticz.com/forum/memberlist.php?mode=viewprofile&u=13749
+# ***** iot_ALARM-SVR.js - Script to manage the CVQ6081 wired Alarm appliance : 
+#       - arm and disarm the Alarm using the Raspberry GPIO/Relay as a keyswitch
+#       - get alarm Alert state at CVQ6081 backpanel
+#
+[submodule "iot_ALARM-SVR.js"]
+	path = Alarm Server/iot_ALARM-SVR.js
+	url = https://github.com/Antori91/Home_Alarm/tree/main/Alarm_nodeJS/iot_ALARM-SVR.js
+	ignore = dirty
